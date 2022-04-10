eed534be51ee8d90f44136e5c0e6fffe168d8c8f
EngJao89 <eng_joao@outlook.com>
2022-04-10 18:08:23 -0300

    Closes 02:"Implementa configuração do typeorm."

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
+import "reflect-metadata";
 import express from "express";
+
+import "./database";
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  return res.send("Hello NLW!");
-});
-
-app.post("/test-post", (req, res) => {
-  return res.send("Hello NLW! Method: POST");
-});
 
 app.listen(3200, () => {
   console.log("Server is running on port 3200");
